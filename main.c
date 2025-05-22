6f20deeb97295504fc3ef15adc2f3d351f793b24
machine-enslaver <71637605+machine-enslaver@users.noreply.github.com>
2025-05-22 11:54:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ int main() {
     for (int i = 2; i < n; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
     }
-
+    
+    // print numbers
     printf("First %d Fibonacci numbers:\n", n);
     for (int i = 0; i < n; i++) {
         printf("%d ", fib[i]);
